d8ca894d919ad7aab4bd042c4c5d300cd482dce5
AlexVishnev <123gibson321@gmail.com>
2018-11-29 14:35:34 +0200

    Push task

---
+++ CMakeFiles/primes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/primes.dir/daemon.c.o"
+  "primes.pdb"
+  "primes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/primes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/test_daemon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_daemon.dir/daemon.c.o"
+  "test_daemon.pdb"
+  "test_daemon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_daemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
